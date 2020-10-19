42ca997b1a4178fbffad9932de9c6224d5c1738f
Jake Wickstrom <jake.wickstrom@lifebooster.ca>
2020-10-18 17:45:01 -0700

    quick setup for heroku

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   var currentTime = new Date();
-  res.send('%s', currentTime)
+  res.json(currentTime)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
-
-
