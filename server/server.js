9960f70249fbe908389e82f6571c3240ddc403e8
Dana Harlos <danaharlos@gmail.com>
2020-10-19 07:46:00 -0700

    added basic structure of where helper functions and API call end points will go

--- server/server.js
+++ server/server.js
@@ -2,11 +2,28 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const community = require('./communityAPIFunctions.js');
+const exercise = require('./exerciseAPIFunctions.js');
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
 app.get('/', (req, res) => {
   var currentTime = new Date();
   res.json(currentTime)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+
+
+//////////  COMMUNITY API CALLS   //////////
+app.get('/community/users', (req, res) => {
+  var result = myModule.users();
+  res.json(result)
 })
+
+
+
+
+
+//////////  EXERCISE API CALLS   //////////
