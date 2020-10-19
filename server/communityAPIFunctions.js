e5b5f640a007d2d463493fc462dee60a0dc3c0f8
Dana Harlos <danaharlos@gmail.com>
2020-10-19 10:35:27 -0700

    Created user call skeleton

--- server/communityAPIFunctions.js
+++ server/communityAPIFunctions.js
@@ -3,7 +3,36 @@ const app = express()
 const port = process.env.PORT || 3000
 
 module.exports = {
-   users: function() {
-      return "This is community users";
+   getUsers: function() {
+      var Users = {
+          user1: {
+            id:  "user1",
+            created: 1,
+            email: "abc@shaw.ca",
+            workouts: 0},
+          user2: {
+            id:  "user2",
+            created: 1,
+            email: "def@shaw.ca",
+            workouts: 0}
+      };
+      return Users;
+   }, 
+   
+   getUser: function(userID) {
+      var User = {
+        id:  userID,
+        created: 1,
+        email: "abc@shaw.ca",
+        workouts: 0};
+      return User;
+   }, 
+   
+   deleteUser: function(userID) {
+      return "Deleting user " + userID;
+   },
+   
+   createUser: function(userID) {
+      return "Creating user " + userID;
    }
 }
