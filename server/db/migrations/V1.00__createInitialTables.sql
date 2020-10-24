6ae3d37deb97f1542708ccdfff15448db289e0a3
Zane Frantzen <frantzen.zane@gmail.com>
2020-10-24 14:41:24 -0700

    Refined initial migration script

--- server/db/migrations/V1.00__createInitialTables.sql
+++ server/db/migrations/V1.00__createInitialTables.sql
@@ -1,28 +1,31 @@
-CREATE TABLE user_workout_pref (
-    id INT PRIMARY KEY IDENTITY (1,1)
-);
-
 CREATE TABLE user_multiplier (
-    id INT PRIMARY KEY IDENTITY (1,1)
+    id INT PRIMARY KEY IDENTITY (1,1),
+    arms DECIMAL(4,3) DEFAULT 1.000,
+    legs DECIMAL(4,3) DEFAULT 1.000,
+    chest DECIMAL(4,3) DEFAULT 1.000,
+    back DECIMAL(5,3) DEFAULT 1.000
 );
 
-CREATE TABLE gender (
+CREATE TABLE major_muscle_group (
     id INT PRIMARY KEY IDENTITY (1,1),
-    gender_name VARCHAR (50)
+    name VARCHAR(50)
 );
 
 CREATE TABLE user_profile (
     id INT PRIMARY KEY IDENTITY (1,1),
-    real_name VARCHAR (50),
-    user_name VARCHAR (50),
-    email VARCHAR (50),
-    age INT,
-    user_weight DECIMAL (5,2),
-    height DECIMAL (5,2),
-    gender_id INT,
+    name VARCHAR(50),
+    user_name VARCHAR(50),
+    email VARCHAR(50),
     user_multiplier_id INT,
-    user_workout_pref_id INT,
-    FOREIGN KEY (gender_id) REFERENCES gender (id),
-    FOREIGN KEY (user_multiplier_id) REFERENCES user_multiplier (id),
-    FOREIGN KEY (user_workout_pref_id) REFERENCES user_workout_pref (id)
+    FOREIGN KEY (user_multiplier_id) REFERENCES user_multiplier (id)
+);
+
+CREATE TABLE exercise (
+    id INT PRIMARY KEY IDENTITY (1,1),
+    name VARCHAR(50),
+    description VARCHAR(200),
+    std_reps INT,
+    std_reps_time_sec INT,
+    major_muscle_group_id INT,
+    FOREIGN KEY (major_muscle_group_id) REFERENCES major_muscle_group (id)
 );
