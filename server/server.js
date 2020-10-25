cefa4a5965e4ec8da11359397dcf290b484da709
Zane Frantzen <43505724+zfrantzen@users.noreply.github.com>
2020-10-25 13:50:08 -0700

    Workout Plan Generation & Multiplier Endpoints (#11)
    * Inserted data and changed structure of db for algo
    * Implemented the workout plan algorithm
    * Refactored files for organization
    * Added API to change workout multipliers
    * Added column to user profile table
    * Removed inline db credentials and improved error handling

--- server/server.js
+++ server/server.js
@@ -5,9 +5,14 @@ const port = process.env.PORT || 3000
 
 const community = require('./communityAPIFunctions.js');
 const exercise = require('./exerciseAPIFunctions.js');
+const workout = require('./src/service/workoutService.js');
+const db = require('./src/connection.js');
+const constants = require('./src/constants.js');
+
+const dbConfig = db.getDbConfig();
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Spottr API listening at http://localhost:${port}`)
 })
 
 app.get('/', cors(), (req, res) => {
@@ -38,4 +43,50 @@ app.delete('/users/:userID', cors(), function (req, res) {
 })
 
 
-//////////  EXERCISE API CALLS   //////////
+//////////  WORKOUT API CALLS   //////////
+app.get('/users/:userId/workout-plan/generate/:lengthMinutes&:targetMuscleGroup', cors(), async function (req, res) {
+  try {
+    var result = await workout.generateWorkoutPlan(
+        JSON.parse(req.params.userId), 
+        JSON.parse(req.params.lengthMinutes), 
+        JSON.parse(req.params.targetMuscleGroup), 
+        dbConfig
+      );
+
+      res.json(result);
+  } catch(ex) {
+    res.status(constants.ERROR_RESPONSE).send(ex);
+  }
+})
+
+app.put('/users/:userId/workout-difficulty/increase/:factor&:targetMuscleGroup', cors(), async function (req, res) {
+  try {
+    var result = await workout.modifyWorkoutDifficulty(
+    JSON.parse(req.params.userId),
+    JSON.parse(req.params.targetMuscleGroup),
+    JSON.parse(req.params.factor),
+    dbConfig,
+    1
+  );
+
+  res.sendStatus(result);
+  } catch(ex) {
+    res.status(constants.ERROR_RESPONSE).send(ex);
+  }
+})
+
+app.put('/users/:userId/workout-difficulty/decrease/:factor&:targetMuscleGroup', cors(), async function (req, res) {
+  try {
+    var result = await workout.modifyWorkoutDifficulty(
+    JSON.parse(req.params.userId),
+    JSON.parse(req.params.targetMuscleGroup),
+    JSON.parse(req.params.factor),
+    dbConfig,
+    0
+  );
+
+  res.sendStatus(result);
+  } catch(ex) {
+    res.status(constants.ERROR_RESPONSE).send(ex);
+  }
+})
---
+++ server/src/data/workoutData.js
@@ -0,0 +1,133 @@
+var sql = require("mssql");
+
+module.exports = {
+    getExercisesByTargetMuscleGroups: function(targetMuscleGroup, dbConfig) {
+        try {
+            return sql
+              .connect(dbConfig)
+              .then((pool) => {
+                return pool
+                  .request()
+                  .input("targetMuscleGroup", sql.Int, targetMuscleGroup)
+                  .query(
+                    "SELECT * FROM exercise WHERE major_muscle_group_id = @targetMuscleGroup"
+                  );
+              })
+              .then((result) => {
+                if (result.recordset.length == 0) throw ('No exercises found for given target muscle group: ' + targetMuscleGroup);
+                return result.recordset;
+              })
+        } catch(ex) {
+            console.log(ex);
+            throw ex;
+        }
+    },
+
+    getUserMultiplier: function(targetMuscleGroup, multiplierId, dbConfig) {
+        try {
+            return sql
+              .connect(dbConfig)
+              .then((pool) => {
+                // Should probably be checking the database instead of hardcoding this,
+                // but it is okay for now... Will need to update db structure to fix properly
+                if (targetMuscleGroup == 1) {
+                    return pool
+                  .request()
+                  .input("multiplierId", sql.Int, multiplierId)
+                  .query(
+                    "SELECT arms FROM user_multiplier WHERE id = @multiplierId"
+                  );
+                } else {
+                    throw 'Invalid requested targetMuscleGroup value! Only muscleGroupId 1 is being handled!';
+                }
+              })
+              .then((result) => {
+                if (result.recordset.length == 0) throw ('No user multiplier records found for given user with multiplier id: ' + multiplierId);
+                return result.recordset[0]['arms'];
+              })
+        } catch(ex) {
+            console.log(ex);
+            throw ex;
+        }
+    },
+
+    createWorkoutPlanEntry: function(userId, dbConfig) {
+        try {
+            return sql
+              .connect(dbConfig)
+              .then((pool) => {
+                return pool
+                  .request()
+                  .input("userId", sql.Int, userId)
+                  .query(
+                    "INSERT INTO workout_plan(user_profile_id) OUTPUT Inserted.id VALUES (@userId)"
+                  );
+              })
+              .then((result) => {
+                return result.recordset[0]['id'];
+              })
+        } catch(ex) {
+            console.log(ex);
+            throw ex;
+        }
+    },
+
+    createWorkoutExerciseEntries: function(workoutPlan, dbConfig) {
+      try {
+        return sql
+          .connect(dbConfig)
+          .then((pool) => {
+            var lastPool;
+
+            for (var i = 0; i < workoutPlan['num_exercises'] * 2; i += 2) {
+              lastPool = pool
+                          .request()
+                          .input("wpid", sql.Int, workoutPlan['workout_plan_id'])
+                          .input("eid", sql.Int, workoutPlan[i]['exercise_id'])
+                          .input("nr", sql.Int, workoutPlan[i]['reps'])
+                          .input("ns", sql.Int, workoutPlan[i]['sets'])
+                          .query(
+                            "INSERT INTO workout_exercise(workout_plan_id, exercise_id, num_reps, num_sets) VALUES (@wpid, @eid, @nr, @ns)"
+                          );
+            }
+
+            return lastPool;
+          })
+          .then((result) => {
+            return result;
+          })
+      } catch(ex) {
+          console.log(ex);
+          throw ex;
+      }
+  },
+
+  upsertNewMultiplier : function(targetMuscleGroup, newMultiplier, userMultiplierId, dbConfig) {
+    try {
+      return sql
+        .connect(dbConfig)
+        .then((pool) => {
+          // Should probably be checking the database instead of hardcoding this,
+          // but it is okay for now... Will need to update db structure to fix properly
+          if (targetMuscleGroup == 1) {
+            return pool
+              .request()
+              .input("umid", sql.Int, userMultiplierId)
+              .input("nm", sql.Decimal(4,3), newMultiplier)
+              .query(
+                "UPDATE user_multiplier SET arms = @nm WHERE id = @umid"
+              );
+          } else {
+              throw 'Invalid requested targetMuscleGroup value! Only muscleGroupId 1 is being handled!';
+          }
+        })
+        .then((result) => {
+          return result;
+        })
+    } catch(ex) {
+        console.log(ex);
+        throw ex;
+    }
+  }
+  
+}
