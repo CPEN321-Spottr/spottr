6639febbf190f250722c3f76dd5458941b290e85
Zane Frantzen <frantzen.zane@gmail.com>
2020-10-25 13:56:35 -0700

    Small hotfix for db connection file

--- server/src/connection.js
+++ server/src/connection.js
@@ -1,16 +1,17 @@
 const util = require('./util.js');
 
 const connectionData = {
-    dbUser: process.env.DB_USER,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     server: process.env.DB_SERVER,
     database: process.env.DB_DATABASE
 };
 
-// Note: An error will occur here if you are trying to run the app locally and have not replaced
+// Note: An exception will occur here if you are trying to run the app locally and have not replaced
 // the "const dbConfig" line in server.js as specified in the Google Doc file
 module.exports = {
     getDbConfig : function () {
-        return util.clone(cloneconnectionData);
+        if (connectionData.length == 0) throw 'Invalid connection data!';
+        return util.clone(connectionData);
     }
 }
