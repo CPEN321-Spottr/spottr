3a587e80c3b6819e9ae391647b5af0ad48845409
Dana Harlos <danaharlos@gmail.com>
2020-10-25 22:20:31 -0700

    Firebased messaging implementation, not working

--- server/src/data/tokenData.js
+++ server/src/data/tokenData.js
@@ -8,7 +8,7 @@ module.exports = {
         //Verify user token
         const ticket = await client.verifyIdToken({
             idToken: token,
-            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+            audience: CLIENT_ID,
         });
         const payload = ticket.getPayload(); 
         return payload
