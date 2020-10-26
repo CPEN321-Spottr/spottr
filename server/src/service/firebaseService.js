bd5fb9e66a1db2bf4466bb5e93a76b98e333d196
Dana Harlos <34802145+danaharlos@users.noreply.github.com>
2020-10-26 04:08:23 -0700

    Firebased messaging implementation & user signin changes (#14)
    * fixed missing data issue during new user creation
    * implemented firebase with fake message sending
    * added some improved error checking to firebase endpoint
    * added console stacktrace to firebase token endpoint
    * moved firebase initialization
    * fixed some bugs with the new user process
    
    Co-authored-by: Zane Frantzen <frantzen.zane@gmail.com>

---
+++ server/src/service/firebaseService.js
@@ -0,0 +1,48 @@
+const util = require('../util.js');
+const constants = require('../constants.js');
+
+var faker = require('faker');
+var admin = require("firebase-admin");
+
+
+module.exports = {
+    firebaseTokenVerify: async function(registrationToken) {
+        return generateNotificationBatch(admin, registrationToken);
+    }
+}
+
+const NOTIFICATIONS_TO_SEND = 12;
+const TIME_BETWEEN_SEC = 2;
+
+async function generateNotificationBatch(admin, registrationToken) {
+  for (var i = 0; i < NOTIFICATIONS_TO_SEND; i++) {
+    // generate message with Faker
+    var payload = {
+      data: {
+        profile_img_uri: faker.image.imageUrl(),
+        name: faker.name.findName(),
+        posted: faker.time.recent().toString()
+      }
+    };
+  
+    var options = {
+      priority: "high",
+      timeToLive: 60 * 60 * 24
+    };
+    
+    // send message
+    admin.messaging().sendToDevice(registrationToken, payload, options)
+      .then(function(response) {
+        console.log("Successfully sent message:", response);
+      })
+      .catch(function(error) {
+        console.log("Error sending message:", error);
+      });
+
+    // generate time to wait before next message (standard time +- 1 sec) and execute it
+    var timeToWait = Math.floor(Math.random() * 2) - 1 + TIME_BETWEEN_SEC;
+    await util.sleep(timeToWait * 1000);
+  }
+
+  return constants.SUCCESS_RESPONSE;
+}
