cda81c3aaeb77112d566e6f1d7d9c35e750440d3
Dana Harlos <danaharlos@gmail.com>
2020-10-29 16:04:50 -0700

    jest tests working

--- server/src/__mocks__/userService.js
+++ server/src/__mocks__/userService.js
@@ -1,12 +1,21 @@
 const userMultiplierData = require('../data/userMultiplierData.js');
 const userData = require('../data/userData.js');
 
+const users = {
+    1: {name: 'Spottr User 1'},
+    2: {name: 'Spottr User 2'}
+}
+
 module.exports = {
-    createNewUser : async function (sub, email, name, dbConfig) {
-        // Create all the data required for a new user in the database structure
-        return new Promise(function(resolve, reject) {
-            var createdUser = "Successfully created user";
-            resolve(createdUser);
+    getAllUsers : async function (dbConfig) {
+        return new Promise(function(resolve) {
+            var getUsers = "Successfully found all users";
+            resolve(getUsers);
+        });
+    },
+    getUserById : async function (userId, dbConfig) {
+        return new Promise(function(resolve) {
+            resolve(users[userId]);
         });
     }
 }
--- server/src/__tests__/userServiceTest.js
+++ server/src/__tests__/userServiceTest.js
@@ -1,14 +1,15 @@
 var userService = require('../__mocks__/userService.js');
 jest.mock("../data/userData.js");
 
-var dbConfig = {
-    user: 'u0tri2ukfid8bnj',
-    password: 'Udh!v6payG2cTwuVAXvta%0&y',
-    server: 'eu-az-sql-serv1.database.windows.net', 
-    database: 'dkxp1krn55tloca'
-  };
+const connection = require('../connection.js');
+var dbConfig = connection.getDbConfig();
 
-it('test createNewUser', () => {
+it('test getAllUsers', () => {
     expect.assertions(1);
-    return userService.createNewUser("JestTestID","jest@shaw.ca","Jest Test",dbConfig).then(data => expect(data).toEqual("Successfully created user"));
+    return userService.getAllUsers(dbConfig).then(data => expect(data).toEqual("Successfully found all users"));
+});
+
+it('test getUserByUserId', () => {
+  expect.assertions(1);
+  return userService.getUserById(1, dbConfig).then(data => expect(data.name).toEqual('Spottr User 1'));
 });
