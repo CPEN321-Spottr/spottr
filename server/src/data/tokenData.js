7c247ed764bd1aea262a0cbc20b274a9d4124a30
Dana Harlos <danaharlos@gmail.com>
2020-11-09 12:01:02 -0800

    dumbfix

--- server/src/data/tokenData.js
+++ server/src/data/tokenData.js
@@ -1,9 +1,5 @@
 var sql = require("mssql");
-<<<<<<< HEAD
 const connection = require('../connection.js');
-=======
-const connection = require("../connection.js");
->>>>>>> 31b5057de66f161edde57efe40e8fb6c339e810d
 
 module.exports = {
     async verifyToken(client, token) {
@@ -11,11 +7,7 @@ module.exports = {
         //Verify user token
         const ticket = await client.verifyIdToken({
             idToken: token,
-<<<<<<< HEAD
-            audience: client_id,
-=======
             audience: connection.getGoogleAuthClientID(),
->>>>>>> 31b5057de66f161edde57efe40e8fb6c339e810d
         });
         const payload = ticket.getPayload();
         return payload;
