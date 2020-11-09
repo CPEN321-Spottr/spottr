6936b8fdc293ad95090acc183e78d3b92b86af7f
Zane Frantzen <frantzen.zane@gmail.com>
2020-11-09 12:07:43 -0800

    Codacy changes

--- server/src/data/tokenData.js
+++ server/src/data/tokenData.js
@@ -1,9 +1,8 @@
 var sql = require("mssql");
-const connection = require('../connection.js');
+const connection = require("../connection.js");
 
 module.exports = {
     async verifyToken(client, token) {
-        client_id = connection.getGoogleAuthClientID();
         //Verify user token
         const ticket = await client.verifyIdToken({
             idToken: token,
