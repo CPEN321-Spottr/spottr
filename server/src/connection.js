5f38cce18b1f78b00e77ba35c60020cc1e49f235
Dana Harlos <danaharlos@gmail.com>
2020-11-09 09:23:18 -0800

    try 1

--- server/src/connection.js
+++ server/src/connection.js
@@ -23,16 +23,16 @@ module.exports = {
 */
         admin.initializeApp({
             credential: admin.credential.cert({
-                "type": "service_account",
-                "project_id": "spottr-1603580674508",
-                "private_key_id": process.env.FIREBASE_PRIVATEKEY_ID,
-                "private_key": process.env.FIREBASE_PRIVATEKEY.toString,
-                "client_email": "firebase-adminsdk-gfhkd@spottr-1603580674508.iam.gserviceaccount.com",
-                "client_id": "100129969434392598267",
-                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                "token_uri": "https://oauth2.googleapis.com/token",
-                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-                "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-gfhkd%40spottr-1603580674508.iam.gserviceaccount.com"
+                type: "service_account",
+                project_id: "spottr-1603580674508",
+                private_key_id: process.env.FIREBASE_PRIVATEKEY_ID,
+                private_key: process.env.FIREBASE_PRIVATEKEY,
+                client_email: "firebase-adminsdk-gfhkd@spottr-1603580674508.iam.gserviceaccount.com",
+                client_id: "100129969434392598267",
+                auth_uri: "https://accounts.google.com/o/oauth2/auth",
+                token_uri: "https://oauth2.googleapis.com/token",
+                auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+                client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-gfhkd%40spottr-1603580674508.iam.gserviceaccount.com"
               }),
             databaseURL: "sqlserver://eu-az-sql-serv1.database.windows.net:1433;database=dkxp1krn55tloca"
         });
