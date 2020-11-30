63a67d57fafbdcd9e3f6c93709f601d60ddc3a40
Jake Wickstrom <jake.wickstrom@lifebooster.ca>
2020-11-29 16:28:55 -0800

    codacy fixes

--- android/app/src/main/java/com/spottr/spottr/activities/MainActivity.java
+++ android/app/src/main/java/com/spottr/spottr/activities/MainActivity.java
@@ -12,7 +12,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
