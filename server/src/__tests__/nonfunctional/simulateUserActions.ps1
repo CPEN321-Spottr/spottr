32e825895326d9d45fd012ff40fde8f15b4e361b
Jake Wickstrom <jake.wickstrom@lifebooster.ca>
2020-12-02 16:35:33 -0800

    non functional all pass

--- server/src/__tests__/nonfunctional/simulateUserActions.ps1
+++ server/src/__tests__/nonfunctional/simulateUserActions.ps1
@@ -9,18 +9,17 @@ $id = New-Guid
 
 $baseURL = "https://spottr-be.herokuapp.com"
 
-$endpoint_types = @("GET", "PUT")
+$endpoint_types = @("GET")
 
 $GET_endpoints = @(
   "$($baseURL)/users",
-  "$($baseURL)/users/6",
-  "$($baseURL)/workout/history/500/1?",
-  "$($baseURL)/workout/workoutplan/1",
-  "$($baseURL)/workout/muscleGroups",
-  "$($baseURL)/workout/muscleGroups",
-  "$($baseURL)/users/1/workout/one-up/1"
+  "$($baseURL)/users/46",
+  "$($baseURL)/workout/history?entries=10",
+  "$($baseURL)/workout/plan/2464",
+  "$($baseURL)/workout/muscle-groups",
+  "$($baseURL)/users/46/workout/one-up/2464"
 )
-$PUT_endpoints = @("$($baseURL)/users/6/workout/change-difficulty/-1&1", "$($baseURL)/users/6/workout/change-difficulty/2&1")
+$PUT_endpoints = @("$($baseURL)/users/46/workout/change-difficulty?factor=-1&target-muscle-group=1", "$($baseURL)/users/46/workout/change-difficulty?factor=1&target-muscle-group=1")
 
 Write-Output "Started User Simulation: $($id)"
 
@@ -51,6 +50,7 @@ while((New-TimeSpan -Start $start -End $current).TotalSeconds -lt $duration) {
     $calls += 1
     $response = Invoke-RestMethod -Uri $endpoint -Method $endpoint_type
   } catch {
+    Write-Host $_
     $failures += 1
   }
 
